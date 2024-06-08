ca13ef3f01fa6ee74291d41053315af8cd2ed52b
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-06-08 20:59:16 -0300

    feat: create UserInfoController

---
+++ src/main/java/br/com/cadastro/simples/controller/UserInfoController.java
@@ -0,0 +1,17 @@
+package br.com.cadastro.simples.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import br.com.cadastro.simples.dto.user.UserInfoRequest;
+import br.com.cadastro.simples.dto.user.UserInfoResponse;
+
+@RestController
+@RequestMapping("userInfo")
+public class UserInfoController {
+
+    @PostMapping("/create")
+    public UserInfoResponse createUserInfos(@RequestBody UserInfoRequest request) {
+        UserInfoResponse response = new UserInfoResponse(request.getFullName(), request.getUsername(), request.getEmail());
+        return response;
+    }
+}
