1c259d9c26bbdae5341d75eaaf82fcb4a6f37dc1
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-06-09 01:07:39 -0300

    feat: adding configuration RabbitMQ

--- src/main/java/br/com/cadastro/simples/Application.java
+++ src/main/java/br/com/cadastro/simples/Application.java
@@ -1,9 +1,11 @@
 package br.com.cadastro.simples;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class Application {
 
 	public static void main(String[] args) {
