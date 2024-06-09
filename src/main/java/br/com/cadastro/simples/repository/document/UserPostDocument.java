a635d37c008c5a489311d5c32d7c79c10e6ccccf
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-06-09 09:53:24 -0300

    feat: create UserPostDocument

---
+++ src/main/java/br/com/cadastro/simples/repository/document/UserPostDocument.java
@@ -0,0 +1,24 @@
+package br.com.cadastro.simples.repository.document;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(value = "userPost")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPostDocument {
+
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    private String username;
+    private LocalDate date;
+}
